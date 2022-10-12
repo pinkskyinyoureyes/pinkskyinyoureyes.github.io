744b491c0f96b65d00b46ad0b7817d3172fac255
Dzmitry Piskunou <pinkskyinyoureyes@gmail.com>
2022-10-12 15:55:02 +0300

    refactor: formatting js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const linkSignInEl = document.querySelector('#link_sign-in')
 const formSignInEl = document.querySelector('.form_sign-in')
 const linkSignUpEl = document.querySelector('#link_sign-up')
 const formSignUpEl = document.querySelector('.form_sign-up')
-
-const modalText = document.querySelector('#modal-text')
-
 linkSignInEl.addEventListener('click', change)
 linkSignUpEl.addEventListener('click', change)
-
 function change() {
     formSignInEl.classList.toggle('not_active')
     formSignUpEl.classList.toggle('not_active')
 }
 
+
 //регистрация sign up
 let regexpEmail = /^([a-zA-Z0-9_\-\.]{3,})+\@([a-zA-Z0-9_\-\.]{3,10})+\.([a-zA-Z]{2,5})$/
 let regexpPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+const modalEl = document.querySelector('.modal') //modal
+const modalContentEl = document.querySelector('.modal_content') //modal
+const modalTextEl = document.querySelector('#modal-text')
 const emailSignUp = document.querySelector('#input_sign-up_email')
 const passwordSignUp = document.querySelector('#input_sign-up_password')
 if (regexpEmail.test(emailSignUp.value) && regexpPassword.test(passwordSignUp.value)) {
@@ -24,12 +24,12 @@ if (regexpEmail.test(emailSignUp.value) && regexpPassword.test(passwordSignUp.va
 } else {
     console.log("false")
 }
-
 document.querySelector('#button_sign-up').onclick = registration
 function registration() {
     window.localStorage.setItem('login', JSON.stringify(emailSignUp.value))
     window.localStorage.setItem('password', JSON.stringify(passwordSignUp.value))
 }
+
 
 //авторизация sign in
 document.querySelector('#button_sign-in').onclick = authorization
@@ -39,12 +39,20 @@ function authorization() {
     let emailSignIn = document.querySelector('#input_sign-in_email')
     let passwordSignIn = document.querySelector('#input_sign-in_password')
     if (JSON.parse(emailStorage) === emailSignIn.value && JSON.parse(passwordStorage) === passwordSignIn.value) {
-        // console.log('Успешно')
-        modal.style.display = 'block'
-        modalText.innerText = 'Вход выполнен успешно!'
+        console.log('Успешно')
+        modalEl.style.display = 'block'
+        modalTextEl.innerText = 'Вход выполнен успешно!'
     } else {
-        modal.style.display = 'block'
-        modalText.innerText = 'Не верные данные. Попробуйте снова.'
-        // console.log('Не успешно')
+        modalEl.style.display = 'block'
+        modalTextEl.innerText = 'Не верные данные. Попробуйте снова.'
+        console.log('Не успешно')
+    }
+}
+
+
+//модальное окно
+window.onclick = function (event) {
+    if (event.target == modalEl || event.target == modalTextEl || event.target == modalContentEl) {
+        modalEl.style.display = 'none'
     }
 }
